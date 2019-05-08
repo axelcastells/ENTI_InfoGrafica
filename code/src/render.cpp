2bc2808fabf458974daddbd3619c17d9451be8ff
Axel Castells Monllau <axelcastells.dev@gmail.com>
2019-05-08 13:07:54 +0200

    Models prepared for opengl

--- code/src/render.cpp
+++ code/src/render.cpp
@@ -236,315 +236,145 @@ void drawAxis() {
 
 ////////////////////////////////////////////////// CUBE
 namespace Cube {
-GLuint cubeVao;
-GLuint cubeVbo[3];
-GLuint cubeShaders[2];
-GLuint cubeProgram;
-glm::mat4 objMat = glm::mat4(1.f);
+	GLuint cubeVao;
+	GLuint cubeVbo[3];
+	GLuint cubeShaders[2];
+	GLuint cubeProgram;
+	glm::mat4 objMat = glm::mat4(1.f);
 
-extern const float halfW = 0.5f;
-int numVerts = 24 + 6; // 4 vertex/face * 6 faces + 6 PRIMITIVE RESTART
+	extern const float halfW = 0.5f;
+	int numVerts = 24 + 6; // 4 vertex/face * 6 faces + 6 PRIMITIVE RESTART
 
-					   //   4---------7
-					   //  /|        /|
-					   // / |       / |
-					   //5---------6  |
-					   //|  0------|--3
-					   //| /       | /
-					   //|/        |/
-					   //1---------2
-glm::vec3 verts[] = {
-	glm::vec3(-halfW, -halfW, -halfW),
-	glm::vec3(-halfW, -halfW,  halfW),
-	glm::vec3(halfW, -halfW,  halfW),
-	glm::vec3(halfW, -halfW, -halfW),
-	glm::vec3(-halfW,  halfW, -halfW),
-	glm::vec3(-halfW,  halfW,  halfW),
-	glm::vec3(halfW,  halfW,  halfW),
-	glm::vec3(halfW,  halfW, -halfW)
-};
-glm::vec3 norms[] = {
-	glm::vec3(0.f, -1.f,  0.f),
-	glm::vec3(0.f,  1.f,  0.f),
-	glm::vec3(-1.f,  0.f,  0.f),
-	glm::vec3(1.f,  0.f,  0.f),
-	glm::vec3(0.f,  0.f, -1.f),
-	glm::vec3(0.f,  0.f,  1.f)
-};
+						   //   4---------7
+						   //  /|        /|
+						   // / |       / |
+						   //5---------6  |
+						   //|  0------|--3
+						   //| /       | /
+						   //|/        |/
+						   //1---------2
+	glm::vec3 verts[] = {
+		glm::vec3(-halfW, -halfW, -halfW),
+		glm::vec3(-halfW, -halfW,  halfW),
+		glm::vec3(halfW, -halfW,  halfW),
+		glm::vec3(halfW, -halfW, -halfW),
+		glm::vec3(-halfW,  halfW, -halfW),
+		glm::vec3(-halfW,  halfW,  halfW),
+		glm::vec3(halfW,  halfW,  halfW),
+		glm::vec3(halfW,  halfW, -halfW)
+	};
+	glm::vec3 norms[] = {
+		glm::vec3(0.f, -1.f,  0.f),
+		glm::vec3(0.f,  1.f,  0.f),
+		glm::vec3(-1.f,  0.f,  0.f),
+		glm::vec3(1.f,  0.f,  0.f),
+		glm::vec3(0.f,  0.f, -1.f),
+		glm::vec3(0.f,  0.f,  1.f)
+	};
 
-glm::vec3 cubeVerts[] = {
-	verts[1], verts[0], verts[2], verts[3],
-	verts[5], verts[6], verts[4], verts[7],
-	verts[1], verts[5], verts[0], verts[4],
-	verts[2], verts[3], verts[6], verts[7],
-	verts[0], verts[4], verts[3], verts[7],
-	verts[1], verts[2], verts[5], verts[6]
-};
-glm::vec3 cubeNorms[] = {
-	norms[0], norms[0], norms[0], norms[0],
-	norms[1], norms[1], norms[1], norms[1],
-	norms[2], norms[2], norms[2], norms[2],
-	norms[3], norms[3], norms[3], norms[3],
-	norms[4], norms[4], norms[4], norms[4],
-	norms[5], norms[5], norms[5], norms[5]
-};
-GLubyte cubeIdx[] = {
-	0, 1, 2, 3, UCHAR_MAX,
-	4, 5, 6, 7, UCHAR_MAX,
-	8, 9, 10, 11, UCHAR_MAX,
-	12, 13, 14, 15, UCHAR_MAX,
-	16, 17, 18, 19, UCHAR_MAX,
-	20, 21, 22, 23, UCHAR_MAX
-};
+	glm::vec3 cubeVerts[] = {
+		verts[1], verts[0], verts[2], verts[3],
+		verts[5], verts[6], verts[4], verts[7],
+		verts[1], verts[5], verts[0], verts[4],
+		verts[2], verts[3], verts[6], verts[7],
+		verts[0], verts[4], verts[3], verts[7],
+		verts[1], verts[2], verts[5], verts[6]
+	};
+	glm::vec3 cubeNorms[] = {
+		norms[0], norms[0], norms[0], norms[0],
+		norms[1], norms[1], norms[1], norms[1],
+		norms[2], norms[2], norms[2], norms[2],
+		norms[3], norms[3], norms[3], norms[3],
+		norms[4], norms[4], norms[4], norms[4],
+		norms[5], norms[5], norms[5], norms[5]
+	};
+	GLubyte cubeIdx[] = {
+		0, 1, 2, 3, UCHAR_MAX,
+		4, 5, 6, 7, UCHAR_MAX,
+		8, 9, 10, 11, UCHAR_MAX,
+		12, 13, 14, 15, UCHAR_MAX,
+		16, 17, 18, 19, UCHAR_MAX,
+		20, 21, 22, 23, UCHAR_MAX
+	};
 
-const char* cube_vertShader =
-"#version 330\n\
-in vec3 in_Position;\n\
-in vec3 in_Normal;\n\
-out vec4 vert_Normal;\n\
-uniform mat4 objMat;\n\
-uniform mat4 mv_Mat;\n\
-uniform mat4 mvpMat;\n\
-void main() {\n\
-	gl_Position = mvpMat * objMat * vec4(in_Position, 1.0);\n\
-	vert_Normal = mv_Mat * objMat * vec4(in_Normal, 0.0);\n\
-}";
-const char* cube_fragShader =
-"#version 330\n\
-in vec4 vert_Normal;\n\
-out vec4 out_Color;\n\
-uniform mat4 mv_Mat;\n\
-uniform vec4 color;\n\
-void main() {\n\
-	out_Color = vec4(color.xyz * dot(vert_Normal, mv_Mat*vec4(0.0, 1.0, 0.0, 0.0)) + color.xyz * 0.3, 1.0 );\n\
-}";
-void setupCube() {
-	glGenVertexArrays(1, &cubeVao);
-	glBindVertexArray(cubeVao);
-	glGenBuffers(3, cubeVbo);
+	const char* cube_vertShader =
+	"#version 330\n\
+	in vec3 in_Position;\n\
+	in vec3 in_Normal;\n\
+	out vec4 vert_Normal;\n\
+	uniform mat4 objMat;\n\
+	uniform mat4 mv_Mat;\n\
+	uniform mat4 mvpMat;\n\
+	void main() {\n\
+		gl_Position = mvpMat * objMat * vec4(in_Position, 1.0);\n\
+		vert_Normal = mv_Mat * objMat * vec4(in_Normal, 0.0);\n\
+	}";
+	const char* cube_fragShader =
+	"#version 330\n\
+	in vec4 vert_Normal;\n\
+	out vec4 out_Color;\n\
+	uniform mat4 mv_Mat;\n\
+	uniform vec4 color;\n\
+	void main() {\n\
+		out_Color = vec4(color.xyz * dot(vert_Normal, mv_Mat*vec4(0.0, 1.0, 0.0, 0.0)) + color.xyz * 0.3, 1.0 );\n\
+	}";
+	void setupCube() {
+		glGenVertexArrays(1, &cubeVao);
+		glBindVertexArray(cubeVao);
+		glGenBuffers(3, cubeVbo);
 
-	glBindBuffer(GL_ARRAY_BUFFER, cubeVbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);
-	glVertexAttribPointer((GLuint)0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, cubeVbo[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeNorms), cubeNorms, GL_STATIC_DRAW);
-	glVertexAttribPointer((GLuint)1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(1);
-
-	glPrimitiveRestartIndex(UCHAR_MAX);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeVbo[2]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIdx), cubeIdx, GL_STATIC_DRAW);
-
-	glBindVertexArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	cubeShaders[0] = compileShader(cube_vertShader, GL_VERTEX_SHADER, "cubeVert");
-	cubeShaders[1] = compileShader(cube_fragShader, GL_FRAGMENT_SHADER, "cubeFrag");
-
-	cubeProgram = glCreateProgram();
-	glAttachShader(cubeProgram, cubeShaders[0]);
-	glAttachShader(cubeProgram, cubeShaders[1]);
-	glBindAttribLocation(cubeProgram, 0, "in_Position");
-	glBindAttribLocation(cubeProgram, 1, "in_Normal");
-	linkProgram(cubeProgram);
-}
-void cleanupCube() {
-	glDeleteBuffers(3, cubeVbo);
-	glDeleteVertexArrays(1, &cubeVao);
-
-	glDeleteProgram(cubeProgram);
-	glDeleteShader(cubeShaders[0]);
-	glDeleteShader(cubeShaders[1]);
-}
-void updateCube(const glm::mat4& transform) {
-	objMat = transform;
-}
-void drawCube() {
-	glEnable(GL_PRIMITIVE_RESTART);
-	glBindVertexArray(cubeVao);
-	glUseProgram(cubeProgram);
-	glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "objMat"), 1, GL_FALSE, glm::value_ptr(objMat));
-	glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "mv_Mat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_modelView));
-	glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "mvpMat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_MVP));
-	glUniform4f(glGetUniformLocation(cubeProgram, "color"), 0.1f, 1.f, 1.f, 0.f);
-	glDrawElements(GL_TRIANGLE_STRIP, numVerts, GL_UNSIGNED_BYTE, 0);
-
-	glUseProgram(0);
-	glBindVertexArray(0);
-	glDisable(GL_PRIMITIVE_RESTART);
-}
-}
-
-namespace Points {
-	GLuint vao, vbo;
-	GLuint shaders[3];
-	GLuint program;
-	static glm::vec3 positions[POINTS_COUNT];
-	glm::mat4 objMat(1.f);
-
-	int randomizer;
-
-	void setupPoints() {
-
-		for (int i = 0; i < POINTS_COUNT; i++) {
-			positions[i] = glm::vec3(0);
-			//Points::positions[i] = { rand() % SPACE_WIDTH - SPACE_WIDTH/2, 
-			//						rand() % SPACE_HEIGHT, 
-			//						rand() %  SPACE_DEPTH - SPACE_DEPTH/2 };
-		}
-
-
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-		glGenBuffers(1, &vbo);
-
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, cubeVbo[0]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);
 		glVertexAttribPointer((GLuint)0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 		glEnableVertexAttribArray(0);
 
-		shaders[0] = compileShader(ResourcesManager::ReadFile(".\\vertexShader").c_str(), GL_VERTEX_SHADER);
-		shaders[1] = compileShader(ResourcesManager::ReadFile(".\\geometryShader").c_str(), GL_GEOMETRY_SHADER);
-		shaders[2] = compileShader(ResourcesManager::ReadFile(".\\fragmentShader").c_str(), GL_FRAGMENT_SHADER);
+		glBindBuffer(GL_ARRAY_BUFFER, cubeVbo[1]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cubeNorms), cubeNorms, GL_STATIC_DRAW);
+		glVertexAttribPointer((GLuint)1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(1);
 
-		program = glCreateProgram();
-		glAttachShader(program, shaders[0]);
-		glAttachShader(program, shaders[1]);
-		glAttachShader(program, shaders[2]);
-		
-		glBindAttribLocation(program, 0, "in_Position");
-		//glBindAttribLocation(program, 1, "in_Color");
-		
-		linkProgram(program);
+		glPrimitiveRestartIndex(UCHAR_MAX);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeVbo[2]);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIdx), cubeIdx, GL_STATIC_DRAW);
 
+		glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
+		cubeShaders[0] = compileShader(cube_vertShader, GL_VERTEX_SHADER, "cubeVert");
+		cubeShaders[1] = compileShader(cube_fragShader, GL_FRAGMENT_SHADER, "cubeFrag");
+
+		cubeProgram = glCreateProgram();
+		glAttachShader(cubeProgram, cubeShaders[0]);
+		glAttachShader(cubeProgram, cubeShaders[1]);
+		glBindAttribLocation(cubeProgram, 0, "in_Position");
+		glBindAttribLocation(cubeProgram, 1, "in_Normal");
+		linkProgram(cubeProgram);
 	}
+	void cleanupCube() {
+		glDeleteBuffers(3, cubeVbo);
+		glDeleteVertexArrays(1, &cubeVao);
 
-	void updatePoints() {
-		for (int i = 0; i < POINTS_COUNT; i++) {
-			
-			//srand(randomizer+i);
-
-			float dirX = Random(-1, 1);
-			float dirY = Random(-1, 1);
-			float dirZ = Random(-1, 1);
-			glm::vec3 dir = glm::normalize(glm::vec3(dirX, dirY, dirZ));
-			
-			//std::cout << "RAND: " << rand() * .00001f << std::endl;
-			positions[i] += dir * GlobalVars::speed;
-			
-			// CLAMP
-			positions[i].x = glm::clamp(positions[i].x, (float)-(SPACE_WIDTH / 2), (float)SPACE_WIDTH / 2);
-			positions[i].y = glm::clamp(positions[i].y, .0f, (float)SPACE_WIDTH);
-			positions[i].z = glm::clamp(positions[i].z, (float)-(SPACE_DEPTH / 2), (float)SPACE_DEPTH / 2);
-		}
-		randomizer++;
+		glDeleteProgram(cubeProgram);
+		glDeleteShader(cubeShaders[0]);
+		glDeleteShader(cubeShaders[1]);
 	}
-
-	void drawPoints() {
-			glBindVertexArray(vao);
-			glUseProgram(program);
-			glUniformMatrix4fv(glGetUniformLocation(program, "mv_Mat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_modelView));
-
-			glUniformMatrix4fv(glGetUniformLocation(program, "mvpMat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_MVP));
-
-			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-			glPointSize(10);
-
-			for (int i = 0; i < POINTS_COUNT; i++) {
-
-				objMat = glm::translate(glm::mat4(1), positions[i]);
-				glUniformMatrix4fv(glGetUniformLocation(program, "objMat"), 1, GL_FALSE, glm::value_ptr(objMat));
-				glDrawArrays(GL_POINTS, i, 1);
-			}
-
+	void updateCube(const glm::mat4& transform) {
+		objMat = transform;
 	}
-}
-namespace Honeycomb {
-	GLuint vao, vbo;
-	GLuint shaders[3];
-	GLuint program;
-	static glm::vec3 positions[POINTS_COUNT];
-	glm::mat4 objMat(1.f);
+	void drawCube() {
+		glEnable(GL_PRIMITIVE_RESTART);
+		glBindVertexArray(cubeVao);
+		glUseProgram(cubeProgram);
+		glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "objMat"), 1, GL_FALSE, glm::value_ptr(objMat));
+		glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "mv_Mat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_modelView));
+		glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "mvpMat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_MVP));
+		glUniform4f(glGetUniformLocation(cubeProgram, "color"), 0.1f, 1.f, 1.f, 0.f);
+		glDrawElements(GL_TRIANGLE_STRIP, numVerts, GL_UNSIGNED_BYTE, 0);
 
-	void setupPoints() {
-
-		for (int i = 0; i < POINTS_COUNT; i++) {
-			if (i % 2 == 0) {
-				positions[i] = glm::vec3(0, GlobalVars::CUBE_WIDTH * i, 0);
-
-			}
-			else {
-				positions[i] = glm::vec3(GlobalVars::CUBE_WIDTH, GlobalVars::CUBE_WIDTH * i, GlobalVars::CUBE_WIDTH);
-			}
-
-		}
-
-
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-		glGenBuffers(1, &vbo);
-
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-		glVertexAttribPointer((GLuint)0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		glEnableVertexAttribArray(0);
-
-		shaders[0] = compileShader(ResourcesManager::ReadFile(".\\vertexShader").c_str(), GL_VERTEX_SHADER);
-		shaders[1] = compileShader(ResourcesManager::ReadFile(".\\hcGeometryShader").c_str(), GL_GEOMETRY_SHADER);
-		shaders[2] = compileShader(ResourcesManager::ReadFile(".\\fragmentShader").c_str(), GL_FRAGMENT_SHADER);
-
-		program = glCreateProgram();
-		glAttachShader(program, shaders[0]);
-		glAttachShader(program, shaders[1]);
-		glAttachShader(program, shaders[2]);
-
-		glBindAttribLocation(program, 0, "in_Position");
-		//glBindAttribLocation(program, 1, "in_Color");
-
-		linkProgram(program);
-
-
-	}
-
-	void updatePoints(float dt) {
-
-		static float s = 0;
-
-		s += dt;
-
-		if (dt >= TAU) {
-			s = 0;
-		}
-		
-		GlobalVars::MUTATOR = MapValue(cos(s), -1, 1, 0.1f, 0.8f);
-	}
-
-	void drawPoints() {
-		glBindVertexArray(vao);
-		glUseProgram(program);
-		glUniformMatrix4fv(glGetUniformLocation(program, "mv_Mat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_modelView));
-
-		glUniformMatrix4fv(glGetUniformLocation(program, "mvpMat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_MVP));
-
-		glUniformMatrix4fv(glGetUniformLocation(program, "objMat"), 1, GL_FALSE, glm::value_ptr(objMat));
-
-		glUniform1f(glGetUniformLocation(program, "mutationFactor"), GlobalVars::MUTATOR);
-
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		glPointSize(10);
-
-		glDrawArrays(GL_POINTS, 0, POINTS_COUNT);
-		//for (int i = 0; i < POINTS_COUNT; i++) {
-
-		//	objMat = glm::translate(glm::mat4(1), positions[i]);
-		//	glUniformMatrix4fv(glGetUniformLocation(program, "objMat"), 1, GL_FALSE, glm::value_ptr(objMat));
-		//	glDrawArrays(GL_POINTS, i, 1);
-		//}
-
+		glUseProgram(0);
+		glBindVertexArray(0);
+		glDisable(GL_PRIMITIVE_RESTART);
 	}
 }
 
@@ -565,8 +395,6 @@ void GLinit(int width, int height) {
 
 
 	// Setup shaders & geometry
-	Points::setupPoints();
-	Honeycomb::setupPoints();
 	Axis::setupAxis();
 	Cube::setupCube();
 	
@@ -613,8 +441,6 @@ void GLrender(float dt) {
 	}break;
 	case 1:
 	{
-		Honeycomb::updatePoints(dt);
-		Honeycomb::drawPoints();
 
 	}break;
 	case 2:
